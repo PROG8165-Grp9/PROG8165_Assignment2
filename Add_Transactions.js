10476d762bec44ce0b48223eae0e0baf7cfa9803
Lea Monica Belo <lea.belo@delasalle.ph>
2017-02-16 10:21:26 -0500

    Add transaction script

---
+++ Add_Transactions.js
@@ -0,0 +1,15 @@
+﻿var Trans =
+    {
+        "Transactions": [
+           { "TransNo": 1, "TransDesc": "Deposit", "TrasDate": "", "TrasType": "Deposit", "TransAmount": 0.00 },
+           { "TransNo": 2, "TransDesc": "Withdrawal", "TrasDate": "", "TrasType": "Withdrawal", "TransAmount": 0.00 },
+           { "TransNo": 3, "TransDesc": "Deposit", "TrasDate": "", "TrasType": "Deposit", "TransAmount": 0.00 },
+           { "TransNo": 4, "TransDesc": "Deposit", "TrasDate": "", "TrasType": "Deposit", "TransAmount": 0.00 },
+           { "TransNo": 5, "TransDesc": "Withdrawal", "TrasDate": "", "TrasType": "Withdrawal", "TransAmount": 0.00 }
+        ]
+    };
+
+for (i in Trans.Transactions) {
+    var obj = Trans[i];
+    document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML + Trans.Transactions[i].TransDesc + "\n";
+}
