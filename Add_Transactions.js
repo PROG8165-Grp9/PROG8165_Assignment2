6fb496b9f3bb93fa5e64c591c7d76a5d2db581b0
Lea Monica Belo <lea.belo@delasalle.ph>
2017-02-19 17:44:32 -0500

    Modified Design

--- Add_Transactions.js
+++ Add_Transactions.js
@@ -2,10 +2,10 @@
 var Trans =
     {
         "Transactions": [
-           { "TransNo": 1, "TransDesc": "TD Canada Trust: Initial Deposit", "TransLocation":"Toronto", "TransDate": "12/01/2017", "TransType": "Deposit", "TransAmount": 0.00 },
+           { "TransNo": 1, "TransDesc": "TD Canada Trust: Initial Deposit", "TransLocation":"Toronto", "TransDate": "12/01/2016", "TransType": "Deposit", "TransAmount": 0.00 },
            { "TransNo": 2, "TransDesc": "Conestoga College ATM Withdrawal", "TransLocation": "Kitchener", "TransDate": "18/04/2016", "TransType": "Withdrawal", "TransAmount": 0.00 },
            { "TransNo": 3, "TransDesc": "TD Canada Trust: ATM Deposit", "TransLocation":"Toronto", "TransDate": "21/06/2016", "TransType": "Deposit", "TransAmount": 0.00 },
-           { "TransNo": 4, "TransDesc": "TD Canada Trust: Pioneer Dr. Branch Deposit", "TransLocation":"Kitchener", "TransDate": "12/7/2016", "TransType": "Deposit", "TransAmount": 0.00 },
+           { "TransNo": 4, "TransDesc": "TD Canada Trust: Pioneer Dr. Branch Deposit", "TransLocation":"Kitchener", "TransDate": "12/07/2016", "TransType": "Deposit", "TransAmount": 0.00 },
            { "TransNo": 5, "TransDesc": "Fairview Park Mall ATM Withdrawal", "TransLocation":"Waterloo", "TransDate": "10/12/2016", "TransType": "Withdrawal", "TransAmount": 0.00 }
         ]
     };
--- TransDet_JS.js
+++ TransDet_JS.js
@@ -7,7 +7,7 @@ for (i in Trans.Transactions) {
     if (Trans.Transactions[i].TransNo == myParam) {
         var parts = (Trans.Transactions[i].TransDate).split('/');
         var mydate = new Date(parts[2], parts[1] - 1, parts[0]);
-        document.getElementById("tbxDate").value = monthNames[mydate.getMonth()];
+        document.getElementById("tbxDate").value = monthNames[mydate.getMonth()] +" " + parts[0] + ", " + parts[2];
         document.getElementById("txtaDesc").value = Trans.Transactions[i].TransDesc;
         document.getElementById("tbxType").value = Trans.Transactions[i].TransType;
         document.getElementById("tbxLoc").value = Trans.Transactions[i].TransLocation;
