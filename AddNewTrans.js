cd200dabb54cf934d5f621b0d11cc3fcb4276747
Lea Monica Belo <lea.belo@delasalle.ph>
2017-02-19 19:05:36 -0500

    Created New Transaction Pages

---
+++ AddNewTrans.js
@@ -0,0 +1,9 @@
+﻿document.getElementById("tbxNTrans").value = Trans.Transactions.length + 1;
+
+function AddTrans() {
+}
+
+function onValueChanged()
+{
+	tbxNAmount.Text = tbxNAmount.value.ToString("#0.00");
+}
--- HeaderScript.js
+++ HeaderScript.js
@@ -1,6 +1,9 @@
 ﻿function LoadDash() {
     document.location.href='Directory.html';
 }
+function CreateTrans() {
+    document.location.href = 'NewTransaction.html';
+}
 function LogOut() {
     document.location.href = 'LogIn.html';
 }
