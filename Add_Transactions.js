ce2d9542b30109886fd00ed1ed1bbf0ea0cea7c1
Lea Monica Belo <lea.belo@delasalle.ph>
2017-02-19 21:51:42 -0500

    Multiple changes to files

--- Add_Transactions.js
+++ Add_Transactions.js
@@ -2,12 +2,33 @@
 var Trans =
     {
         "Transactions": [
-           { "TransNo": 1, "TransDesc": "TD Canada Trust: Initial Deposit", "TransLocation":"Toronto", "TransDate": "12/01/2016", "TransType": "Deposit", "TransAmount": 0.00 },
-           { "TransNo": 2, "TransDesc": "Conestoga College ATM Withdrawal", "TransLocation": "Kitchener", "TransDate": "18/04/2016", "TransType": "Withdrawal", "TransAmount": 0.00 },
-           { "TransNo": 3, "TransDesc": "TD Canada Trust: ATM Deposit", "TransLocation":"Toronto", "TransDate": "21/06/2016", "TransType": "Deposit", "TransAmount": 0.00 },
-           { "TransNo": 4, "TransDesc": "TD Canada Trust: Pioneer Dr. Branch Deposit", "TransLocation":"Kitchener", "TransDate": "12/07/2016", "TransType": "Deposit", "TransAmount": 0.00 },
-           { "TransNo": 5, "TransDesc": "Fairview Park Mall ATM Withdrawal", "TransLocation":"Waterloo", "TransDate": "10/12/2016", "TransType": "Withdrawal", "TransAmount": 0.00 }
+           { "TransNo": 1, "TransDesc": "TD Canada Trust: Initial Deposit", "TransLocation":"Toronto", "TransDate": "12/01/2016", "TransType": "Deposit", "TransAmount": 100.00 },
+           { "TransNo": 2, "TransDesc": "Conestoga College ATM Withdrawal", "TransLocation": "Kitchener", "TransDate": "18/04/2016", "TransType": "Withdrawal", "TransAmount": 20.00 },
+           { "TransNo": 3, "TransDesc": "TD Canada Trust: ATM Deposit", "TransLocation":"Toronto", "TransDate": "21/06/2016", "TransType": "Deposit", "TransAmount": 150.00 },
+           { "TransNo": 4, "TransDesc": "TD Canada Trust: Pioneer Dr. Branch Deposit", "TransLocation":"Kitchener", "TransDate": "12/07/2016", "TransType": "Deposit", "TransAmount": 200.00 },
+           { "TransNo": 5, "TransDesc": "Fairview Park Mall ATM Withdrawal", "TransLocation":"Waterloo", "TransDate": "10/12/2016", "TransType": "Withdrawal", "TransAmount": 70.00 }
         ]
-    };
+	};
 
-    
+var mystring = "[";
+var obj = Trans;
+for (var x in obj.Transactions){
+  var thisstring = "[" + obj.Transactions[x].TransNo + ", \"" + 
+  obj.Transactions[x].TransDesc + "\", \"" + 
+  obj.Transactions[x].TransLocation + "\", \"" + 
+  obj.Transactions[x].TransDate + "\", \"" + 
+  obj.Transactions[x].TransType + "\", " + 
+  obj.Transactions[x].TransAmount + "]"
+  mystring = mystring + thisstring;
+}
+mystring = mystring + "];";
+
+
+//var Transac =
+//    [
+//           [ 1, "TD Canada Trust: Initial Deposit", "Toronto",  "12/01/2016", "Deposit", 0.00 ],
+//           [ 2, "Conestoga College ATM Withdrawal", "Kitchener", "18/04/2016", "Withdrawal", 0.00 ],
+//           [ 3, "TD Canada Trust: ATM Deposit", "Toronto", "21/06/2016", "Deposit", 0.00 ],
+//           [ 4, "TD Canada Trust: Pioneer Dr. Branch Deposit", "Kitchener", "12/07/2016", "Deposit", 0.00 ],
+//           [ 5, "Fairview Park Mall ATM Withdrawal", "Waterloo", "10/12/2016", "Withdrawal", 0.00 ]
+//        ];
