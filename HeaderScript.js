d38ea0fc5ada880aaa48ddb909db9100ffb4efc3
Lea Monica Belo <lea.belo@delasalle.ph>
2017-02-19 22:21:28 -0500

    Adjusted changes

--- HeaderScript.js
+++ HeaderScript.js
@@ -7,3 +7,30 @@ function CreateTrans() {
 function LogOut() {
     document.location.href = 'LogIn.html';
 }
+
+function SelectionChange()
+{
+	var selType = document.getElementById("selectType").value;
+	var selections= document.getElementById("selectOption");
+	if(selType == "Month Range")
+	{
+		var option = document.createElement("option");
+		option.text = "For the last 3 Months";
+		selections.add(option);
+		option.text = "For the last 6 Months";
+		selections.add(option);
+		option.text = "For the last year";
+		selections.add(option);
+	}
+	else if (selType =="Type")
+	{
+		var option = document.createElement("option");
+		option.text = "For the last 3 Months";
+		selections.add(option);
+		option.text = "For the last 6 Months";
+		selections.add(option);
+	}
+	else if(selType == "Location")
+	{
+	}
+}
