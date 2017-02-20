0d784ec6e5f149f4dd93be4ac7e4a60c9cc312a3
Yaroslav Handabura <Yhandabura4038@conestogac.on.ca>
2017-02-20 11:45:56 -0500

    Sorting updates

--- Add_Transactions.js
+++ Add_Transactions.js
@@ -10,19 +10,6 @@ var Trans =
         ]
 	};
 
-var mystring = "[";
-var obj = Trans;
-for (var x in obj.Transactions){
-  var thisstring = "[" + obj.Transactions[x].TransNo + ", \"" + 
-  obj.Transactions[x].TransDesc + "\", \"" + 
-  obj.Transactions[x].TransLocation + "\", \"" + 
-  obj.Transactions[x].TransDate + "\", \"" + 
-  obj.Transactions[x].TransType + "\", " + 
-  obj.Transactions[x].TransAmount + "]"
-  mystring = mystring + thisstring;
-}
-mystring = mystring + "];";
-
 function json2array(json){ 
 var result = []; 
 var keys = Object.keys(json); 
@@ -32,41 +19,7 @@ result.push(json[key]);
 return result; 
 } 
 
-var arr = json2array(Trans); 
-
-function sortDate() { 
-   arr["0"].sort(function(a, b){
-    var dateA=new Date(a.TransDate), dateB=new Date(b.TransDate)
-    return dateA-dateB //sort by date ascending
-});   
-}
-function sortType() {
-    arr["0"].sort(function(a, b){ 
-    var x = a.TransType.toLowerCase(); 
-    var y = b.TransType.toLowerCase(); 
-    if (x < y) {return -1;} 
-    if (x > y) {return 1;} 
-    return 0; 
-    });
-}
-function sortLocation() {
-    arr["0"].sort(function(a, b){ 
-    var x = a.TransLocation.toLowerCase(); 
-    var y = b.TransLocation.toLowerCase(); 
-    if (x < y) {return -1;} 
-    if (x > y) {return 1;} 
-    return 0; 
-    });
-}
-
-//var Transac =
-//    [
-//           [ 1, "TD Canada Trust: Initial Deposit", "Toronto",  "12/01/2016", "Deposit", 0.00 ],
-//           [ 2, "Conestoga College ATM Withdrawal", "Kitchener", "18/04/2016", "Withdrawal", 0.00 ],
-//           [ 3, "TD Canada Trust: ATM Deposit", "Toronto", "21/06/2016", "Deposit", 0.00 ],
-//           [ 4, "TD Canada Trust: Pioneer Dr. Branch Deposit", "Kitchener", "12/07/2016", "Deposit", 0.00 ],
-//           [ 5, "Fairview Park Mall ATM Withdrawal", "Waterloo", "10/12/2016", "Withdrawal", 0.00 ]
-//        ];
+var arr = json2array(Trans);
 
 
 
--- Dir_JS.js
+++ Dir_JS.js
@@ -1,21 +1,54 @@
-var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"];
-
-/**$(document).ready(function() {
-    $('#TransTable').DataTable({
-        data: Transac,
-        columns: [
-            { title: "No." },
-            { title: "Transaction Month" },
-            { title: "Type" },
-            { title: "Location." }
-            
-        ]
-    });
-});**/
-
 var table = document.getElementById("TransTable");
-	for (i in Trans.Transactions) {
+var select = document.getElementById("sort");
+
+function clearTable() {
+    table.replaceChild(document.createElement('TBODY'), table.tBodies[0]);
+}
+
+function sortDate() { 
+   arr["0"].sort(function(a, b){
+    var dateA=new Date(a.TransDate), dateB=new Date(b.TransDate)
+    return dateA-dateB //sort by date ascending    
+});   
+}
+
+function sortType() {
+    arr["0"].sort(function(a, b){ 
+    var x = a.TransType.toLowerCase(); 
+    var y = b.TransType.toLowerCase(); 
+    if (x < y) {return -1;} 
+    if (x > y) {return 1;} 
+    return 0; 
+    });  
+    
+}
+
+function sortLocation() {
+    arr["0"].sort(function(a, b){ 
+    var x = a.TransLocation.toLowerCase(); 
+    var y = b.TransLocation.toLowerCase(); 
+    if (x < y) {return -1;} 
+    if (x > y) {return 1;} 
+    return 0; 
+    });
+}
+
+function sortTable() {
+    if(select.selectedIndex == 0) {
+        sortDate();
+        updateItems();
+    }
+    else if(select.selectedIndex == 1) {
+        sortType();
+        updateItems();
+    }
+    else if(select.selectedIndex == 2) {
+        sortLocation();
+        updateItems();
+    } 
+}  
+
+for (i in Trans.Transactions) {
 	    var row = table.insertRow(table.getElementsByTagName("tr").length-1);
 	    var TransNum = row.insertCell(0);
 		var TransDesc = row.insertCell(1);
@@ -33,9 +66,34 @@ var table = document.getElementById("TransTable");
         var mydate = new Date(parts[2], parts[1] - 1, parts[0]);
 		TransDate.innerHTML = Trans.Transactions[i].TransDate;
 		TransType.innerHTML = Trans.Transactions[i].TransType;
+		row.id = Trans.Transactions[i].TransNo;    
+    }
 
-		row.id = Trans.Transactions[i].TransNo;
-		
-	}
+function updateItems() {  
+    clearTable();
+    for (i in Trans.Transactions) {	
+        var row = table.insertRow(table.getElementsByTagName("tr").length);
+	    var TransNum = row.insertCell(0);
+		var TransDesc = row.insertCell(1);
+		var TransDate = row.insertCell(2);
+		var TransType = row.insertCell(3);
+		var TransLoc = row.insertCell(4);
+		var link = document.createElement('a');
+		link.setAttribute('href', "TransacDetails.html?TransNo=" + Trans.Transactions[i].TransNo);
+		TransNum.appendChild(link);
+		link.setAttribute('target', "_self");
+		link.innerHTML = Trans.Transactions[i].TransNo;
+		TransDesc.innerHTML = Trans.Transactions[i].TransDesc;
+		TransLoc.innerHTML = Trans.Transactions[i].TransLocation;
+		TransDate.innerHTML = Trans.Transactions[i].TransDate;
+		TransType.innerHTML = Trans.Transactions[i].TransType;
+		row.id = Trans.Transactions[i].TransNo;          
+        }
+}
+
+
+
+
+
 
 
